18d523b29a1545e15260225b8542f1320edc7576
nurrido <nurido1445@gmail.com>
2025-09-15 11:47:30 +0700

    membuat layouts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,75 +1,73 @@
 <!doctype html>
-<html lang="en" class="light-style layout-wide customizer-hide"
-      dir="ltr"
-      data-theme="theme-default"
-     data-assets-path="../../assets/"
-      data-template="vertical-menu-template"
-      data-style="light">
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-  <title>@yield('title', 'Vuexy - Laravel')</title>
-  <meta name="description" content="@yield('meta_description', 'Vuexy Template')" />
+    <title>Login | WISATAKU</title>
 
-  <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="{{ assets('assets/img/favicon/favicon.ico') }}" />
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon/favicon.ico') }}" />
 
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&display=swap"
-        rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
+      rel="stylesheet" />
 
-  <!-- Icons -->
-  <link rel="stylesheet" href="{{ assets('vendor/fonts/fontawesome.css') }}" />
-  <link rel="stylesheet" href="{{ assets('vendor/fonts/tabler-icons.css') }}" />
-  <link rel="stylesheet" href="{{ assets('vendor/fonts/flag-icons.css') }}" />
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{ asset('vendor/fonts/fontawesome.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/fonts/tabler-icons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/fonts/flag-icons.css') }}" />
 
-  <!-- Core CSS -->
-  <link rel="stylesheet" href="{{ assets('vendor/css/rtl/core.css') }}" class="template-customizer-core-css" />
-  <link rel="stylesheet" href="{{ assets('vendor/css/rtl/theme-default.css') }}" class="template-customizer-theme-css" />
-  <link rel="stylesheet" href="{{ assets('css/demo.css') }}" />
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('vendor/css/rtl/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('vendor/css/rtl/theme-default.css') }}" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ asset('css/demo.css') }}" />
 
-  <!-- Vendors CSS -->
-  <link rel="stylesheet" href="{{ assets('vendor/libs/node-waves/node-waves.css') }}" />
-  <link rel="stylesheet" href="{{ assets('vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
-  <link rel="stylesheet" href="{{ assets('vendor/libs/typeahead-js/typeahead.css') }}" />
-  <link rel="stylesheet" href="{{ assets('vendor/libs/@form-validation/form-validation.css') }}" />
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{ asset('vendor/libs/node-waves/node-waves.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/libs/typeahead-js/typeahead.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/libs/@form-validation/form-validation.css') }}" />
 
-  <!-- Page CSS -->
-  @stack('page-css')
+    <!-- Page CSS -->
+    <link rel="stylesheet" href="{{ asset('vendor/css/pages/page-auth.css') }}" />
 
-  <!-- Helpers -->
-  <script src="{{ assets('vendor/js/helpers.js') }}"></script>
-  <script src="{{ assets('vendor/js/template-customizer.js') }}"></script>
-  <script src="{{ assets('js/config.js') }}"></script>
-</head>
+    <!-- Helpers -->
+    <script src="{{ asset('vendor/js/helpers.js') }}"></script>
+    <script src="{{ asset('vendor/js/template-customizer.js') }}"></script>
+    <script src="{{ asset('js/config.js') }}"></script>
+  </head>
 
-<body>
-  @yield('content')
+  <body>
+    <!-- Content -->
+    @yield('content')
+    <!-- / Content -->
 
-  <!-- Core JS -->
-  <script src="{{ asset('vendor/libs/jquery/jquery.js') }}"></script>
-  <script src="{{ asset('vendor/libs/popper/popper.js') }}"></script>
-  <script src="{{ asset('vendor/js/bootstrap.js') }}"></script>
-  <script src="{{ asset('vendor/libs/node-waves/node-waves.js') }}"></script>
-  <script src="{{ asset('vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
-  <script src="{{ asset('vendor/libs/hammer/hammer.js') }}"></script>
-  <script src="{{ asset('vendor/libs/i18n/i18n.js') }}"></script>
-  <script src="{{ asset('vendor/libs/typeahead-js/typeahead.js') }}"></script>
-  <script src="{{ asset('vendor/js/menu.js') }}"></script>
+    <!-- Core JS -->
+    <script src="{{ asset('vendor/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('vendor/libs/node-waves/node-waves.js') }}"></script>
+    <script src="{{ asset('vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('vendor/libs/hammer/hammer.js') }}"></script>
+    <script src="{{ asset('vendor/libs/i18n/i18n.js') }}"></script>
+    <script src="{{ asset('vendor/libs/typeahead-js/typeahead.js') }}"></script>
+    <script src="{{ asset('vendor/js/menu.js') }}"></script>
 
-  <!-- Vendors JS -->
-  <script src="{{ assets('vendor/libs/@form-validation/popular.js') }}"></script>
-  <script src="{{ assets('vendor/libs/@form-validation/bootstrap5.js') }}"></script>
-  <script src="{{ assets('vendor/libs/@form-validation/auto-focus.js') }}"></script>
+    <!-- Vendors JS -->
+    <script src="{{ asset('vendor/libs/@form-validation/popular.js') }}"></script>
+    <script src="{{ asset('vendor/libs/@form-validation/bootstrap5.js') }}"></script>
+    <script src="{{ asset('vendor/libs/@form-validation/auto-focus.js') }}"></script>
 
-  <!-- Main JS -->
-  <script src="{{ assets('js/main.js') }}"></script>
+    <!-- Main JS -->
+    <script src="{{ asset('js/main.js') }}"></script>
 
-  <!-- Page JS -->
-  @stack('page-js')
-</body>
+    <!-- Page JS -->
+    <script src="{{ asset('js/pages-auth.js') }}"></script>
+  </body>
 </html>
